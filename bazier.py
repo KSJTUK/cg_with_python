541e7a05531717ebbf5906756beea3ed1d5512a7
KimsungjoonMyLaptop <lastbabo2480@tukorea.ac.kr>
2023-11-05 01:41:22 +0900

    4차 배지어 곡선 그리기 완성 및 스크린 너비, 높이를 조정할 수 있게 변경

--- bazier.py
+++ bazier.py
@@ -3,6 +3,13 @@ import random
 import turtle
 import math
 
+screen = turtle.Screen()
+w, h = 1600, 800
+
+screen.setup(width = w, height = h, startx = 0, starty = 0)
+h -= 50
+point_minmax = ((-w // 2, w // 2), (-h // 2, h // 2))
+
 # 터틀 모듈을 사용해서 라인 그리기, 시점 종점에 좌표값을 찍음
 def draw_line_turtle(start, end, print_point = True, draw_stamp = True):
 	turtle.penup()
@@ -19,7 +26,7 @@ def draw_line_turtle(start, end, print_point = True, draw_stamp = True):
 		turtle.write(end)
 
 def get_random_control_point(point_count, minmaxX, minmaxY):
-	if point_count > 5 or point_count < 3:
+	if point_count > 6 or point_count < 3:
 		return None
 	return [(random.randint(*minmaxX), random.randint(*minmaxY)) for _ in range(point_count)]
 	
@@ -106,10 +113,41 @@ def bazier_curve_3d(control_point_list, step):
 		t += step
 	return point_list
 
+def bazier_curve_3d(control_point_list, step):
+	if step >= 1.0 or step <= 0.0:
+		raise ValueError(f'function bazier_curve_1d: step value error, step value is: {step}\n \
+			step value must be (0.0, 1.0)')
+
+	t = 0.0
+	point_list = []
+
+	p0 = control_point_list[0]
+	p1 = control_point_list[1]
+	p2 = control_point_list[2]
+	p3 = control_point_list[3]
+	p4 = control_point_list[4]
+	p5 = control_point_list[5]
+
+	running = True
+	while running:
+		if t > 1.0:
+			t = 1.0
+			running = False
+
+		p05 = [0, 0]
+		p05[0] = pow((1 - t), 5) * p0[0] + 5 * t * pow((1 - t), 4) * p1[0] + 10 * pow(t, 2) * pow((1 - t), 3) * p2[0] \
+			+ 10 * pow(t, 3) * pow((1 - t), 2) * p3[0] + 5 * pow(t, 4) * (1 - t) * p4[0] + pow(t, 5) * p5[0]
+		p05[1] = pow((1 - t), 5) * p0[1] + 5 * t * pow((1 - t), 4) * p1[1] + 10 * pow(t, 2) * pow((1 - t), 3) * p2[1] \
+			+ 10 * pow(t, 3) * pow((1 - t), 2) * p3[1] + 5 * pow(t, 4) * (1 - t) * p4[0] + pow(t, 5) * p5[1]
+		point_list.append(p05)
+
+		t += step
+	return point_list
+
 
 def get_bazier_curve(control_point_list, step):
 	length_array = len(control_point_list)
-	if length_array < 3 or length_array > 5:
+	if length_array < 3 or length_array > 6:
 		return None
 
 	point_list = None
@@ -120,17 +158,19 @@ def get_bazier_curve(control_point_list, step):
 			point_list = bazier_curve_2d(control_point_list, step)
 		case 5:
 			point_list = bazier_curve_3d(control_point_list, step)
+		case 6:
+			point_list = bazier_curve_3d(control_point_list, step)
 
 	return point_list
 
 
 
-control_point_list = get_random_control_point(5, (-400, 400), (-300, 300))
+control_point_list = get_random_control_point(6, point_minmax[0], point_minmax[1])
 
 for i in range(len(control_point_list) - 1):
 	draw_line_turtle(control_point_list[i], control_point_list[i + 1])
 
-bazier_point_list = get_bazier_curve(control_point_list, 0.05)
+bazier_point_list = get_bazier_curve(control_point_list, 0.01)
 
 turtle.color('blue')
 for i in range(len(bazier_point_list) - 1):
