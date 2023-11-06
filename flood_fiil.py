162947723c5a455101630511926de7073b778b40
KimsungjoonMyLaptop <lastbabo2480@tukorea.ac.kr>
2023-11-06 23:02:22 +0900

    범람 영영채우기 알고리즘 완성

--- flood_fiil.py
+++ flood_fiil.py
@@ -1,23 +1,54 @@
 import turtle
 import bresenham_algorithm_circle
 
+bgcolor = 0xffffff
+black = 0x000000
+fill_color = 0x0000ff # blue
+
+def floor_fill(target_point):
+	tx, ty = target_point[0], target_point[1]
+	if screen_rgb_map[tx][ty] == bgcolor:
+		screen_rgb_map[tx][ty] = fill_color
+		turtle.goto(tx-HW, ty-HH)
+		floor_fill((tx+1, ty))
+		floor_fill((tx-1, ty))
+		floor_fill((tx, ty+1))
+		floor_fill((tx, ty-1))
+	else:
+		return
+
+def floor_fill_not_recursion(target_point):
+	turtle.penup()
+	tx, ty = target_point[0], target_point[1]
+	while True:
+		if screen_rgb_map[tx][ty] == bgcolor:
+			screen_rgb_map[tx][ty] = fill_color
+			turtle.goto(tx-HW, ty-HH)
+			turtle.stamp()
 
 bresenham_algorithm_circle.setting_screen()
 
 HW = bresenham_algorithm_circle.HALF_W
 HH = bresenham_algorithm_circle.HALF_H
 
-bgcolor = 0xffffff
-black = 0x000000
 screen_rgb_map = [[bgcolor for _ in range(bresenham_algorithm_circle.H)] for _ in range(bresenham_algorithm_circle.W)]
 
 print(len(screen_rgb_map), len(screen_rgb_map[0]), f'{screen_rgb_map[0][0]:0x}')
 
-circle_points = bresenham_algorithm_circle.draw_circle_bresenham((0 ,0), 100, False)
+target_point = (0, 0)
+circle_points = bresenham_algorithm_circle.draw_circle_bresenham(target_point, 30)
 
 for point in circle_points:
 	for x, y in point:
 		screen_rgb_map[x + HW][y + HH] = black
 
-def floor_fill(center, background_color, screen_map):
-	cx, cy = center[0], center[1]
+turtle.color('blue')
+turtle.shape('circle')
+turtle.speed(10)
+turtle.shapesize(0.1)
+
+turtle.goto(*target_point)
+turtle.pendown()
+floor_fill((target_point[0]+HW, target_point[1]+HH))
+
+turtle.exitonclick()
